d7e72bd0c37739f7841e0c67468fe9a00bbe3d22
Anjali Sharma <121468990+aaannjali@users.noreply.github.com>
2025-01-16 14:22:36 +0530

    commit

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,29 @@
+"use strict";
+//It is not a statement, but a literal expression, ignored by earlier versions of JavaScript.
+//The purpose of "use strict" is to indicate that the code should be executed in "strict mode".
+
+//alert(3 + 3); -> we are using node js not browser
+
+let number = 10;
+let str = "Anjali";
+let truth = true;
+let weight = 11.9;
+
+
+console.table([number,str,truth,weight]);
+console.log(typeof(number)); // -> number
+console.log(typeof(null)); // -> representaion of emptytype , it is a object
+
+
+//null -> object
+//undefined -> undefined
+
+
+/* primitive types ->
+undefined
+null
+boolean
+string
+number
+symbol
+*/
