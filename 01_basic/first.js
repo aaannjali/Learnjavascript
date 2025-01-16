702ebf4cd9633e2d64d411f0755bb94e4ca96bd9
Anjali Sharma <121468990+aaannjali@users.noreply.github.com>
2025-01-16 09:24:47 +0530

    commit

---
+++ 01_basic/first.js
@@ -0,0 +1,17 @@
+// THERE ARE THREE TYPES OF WAY TO declare A VARIABLE
+// CONST, VAR , LET
+// YOU CAN NOT CHANGE THE VALUE IN 
+const accountId = 144553
+let accountEmail = "anjali@gmail.com"
+var name = "anjali";
+let account;
+// if you just declare a variable with No value it will give you undefine.
+
+//console.log(accountId);
+console.log("Hello World");
+console.table([accountEmail,accountId,name, account]);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
